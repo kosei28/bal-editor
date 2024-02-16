c763f6e4eca76695922d626f315b9a6518a04cb1
kosei28 <contact@kosei28.com>
2024-02-16 18:17:09 +0900

    scroll to caret

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './app.css';
-import Editor from './Editor';
+import MyEditor from './Editor';
 
 function App() {
     return (
         <div className="container">
             <h1>Bal Editor</h1>
-            <Editor />
+            <MyEditor />
         </div>
     );
 }
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from 'react';
+import { Editor } from '@tiptap/core';
 import { EditorProvider } from '@tiptap/react';
 import Document from '@tiptap/extension-document';
 import Paragraph from '@tiptap/extension-paragraph';
@@ -14,9 +16,42 @@ const extensions = [
     Placeholder.configure({ placeholder: 'Enter some text...' }),
 ];
 
-export default function Editor() {
+export default function MyEditor() {
+    const [scrollTimeoutId, setScrollTimeoutId] = useState<
+        number | undefined
+    >();
+
+    const scrollToCaret = useCallback(
+        (editor: Editor) => {
+            const pos = editor.state.selection.$anchor.pos;
+            const coords = editor.view.coordsAtPos(pos);
+            const diff =
+                coords.top + 24 - document.documentElement.clientHeight / 2;
+
+            if (Math.abs(diff) > 5 && editor.state.selection.empty) {
+                window.scrollBy({
+                    top: diff / 10,
+                });
+
+                const timeoutId = setTimeout(() => scrollToCaret(editor), 10);
+                setScrollTimeoutId(timeoutId);
+            } else {
+                clearTimeout(scrollTimeoutId);
+                setScrollTimeoutId(undefined);
+            }
+        },
+        [scrollTimeoutId]
+    );
+
     return (
-        <EditorProvider extensions={extensions}>
+        <EditorProvider
+            extensions={extensions}
+            onSelectionUpdate={({ editor }) => {
+                if (scrollTimeoutId === undefined) {
+                    scrollToCaret(editor);
+                }
+            }}
+        >
             <></>
         </EditorProvider>
     );
